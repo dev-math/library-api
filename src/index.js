889a933395dce92da4c59c2662e765c660df5537
Matheus Rocha <01devmath@gmail.com>
2023-02-01 19:54:19 -0300

    feat: add TODO routes

--- src/index.js
+++ src/index.js
@@ -6,6 +6,29 @@ initDatabase();
 const app = express();
 const PORT = 8080;
 
+// BOOKS
+// GET /books - Retrieve a list of all books in the library
+// GET /books/:id - Retrieve information about a specific book
+// POST /books - Add a new book to the library
+// PUT /books/:id - Update information about a specific book
+// DELETE /books/:id - Remove a book from the library
+
+// GET: /me current user details
+// /users 
+// POST: create user
+// DELETE: remove user
+// GET: / list users
+// GET: /users/:userId list user
+// PUT: /users/:userId update user
+
+// GET: /users/:userId/booklists list booklists
+// POST: /users/:userId/booklists create booklist
+// GET: /users/:userId/booklists/:id list booklist
+// PUT: /users/:userId/booklists/:id update booklist
+
+// SEARCH
+// GET /search?q=:query - Search for books by title, author, or genre
+
 app.get("/", (_req, res) => {
   res.send("Oi");
 });
