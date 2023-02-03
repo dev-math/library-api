ff30738125a838d014eec3ac5d96f647d1c783a5
Matheus Rocha <01devmath@gmail.com>
2023-02-03 16:16:55 -0300

    fix: try to send response more than one time

--- src/controller/user.js
+++ src/controller/user.js
@@ -53,7 +53,7 @@ const listUser = async (req, res) => {
   try {
     const user = await User.findOne({ _id: req.params.id });
     if (!user) {
-      res.status(404).json({ error: `User not found` });
+      return res.status(404).json({ error: `User not found` });
     }
 
     res.status(200).json({ user });
