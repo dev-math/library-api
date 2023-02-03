4ccee1a0e61f2d5a082836f4f2a69d48c4ffcc0b
Matheus Rocha <01devmath@gmail.com>
2023-02-03 13:32:37 -0300

    feat: hash passwords, add check user data function, add list all users function

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,18 +1,52 @@
+import { compare, genSalt, hash } from "bcrypt";
 import User from "../models/user";
 
 const createUser = async (req, res) => {
   try {
     const isSetted = await User.findOne({ email: req.body.email });
     if (isSetted) {
-      throw new Error("Email exists");
+      throw new Error("Email already exists");
     }
 
     const user = new User(req.body);
+
+    const saltRounds = 10;
+    const salt = await genSalt(saltRounds);
+    user.password = await hash(user.password, salt);
+
     await user.save();
     res.status(201).json({ user });
   } catch (error) {
-    res.stataus(400).send(e);
+    console.log(error);
+    res.status(400).json({ erro: `${error}` });
   }
 };
 
-export { createUser };
+const checkUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const checkPassword = await compare(req.body.password, user.password);
+    if (!checkPassword) {
+      throw new Error("Login incorrect");
+    }
+
+    res.send("logado");
+  } catch (error) {
+    res.status(400).json({ erro: `${error}` });
+  }
+};
+
+const listUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({ users });
+  } catch (error) {
+    res.status(400).json({ erro: `${error}` });
+  }
+};
+
+export { createUser, checkUser, listUsers };
