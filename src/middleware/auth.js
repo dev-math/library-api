a8d534b063126cf46eee000d5558fd5005eca50b
Matheus Rocha <01devmath@gmail.com>
2023-02-03 17:58:06 -0300

    feat: add auth middleware to protect routes

---
+++ src/middleware/auth.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user";
+
+const jwtSecretKey = "secretkey";
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, jwtSecretKey);
+    const user = await User.findOne({ _id: decoded._id });
+
+    if (!user) {
+      throw new Error();
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Please authenticate" });
+  }
+};
+
+export default auth;
