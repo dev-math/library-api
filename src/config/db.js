4ee66ac66d8afb0126637621a25cef0a46d954bf
Matheus Rocha <01devmath@gmail.com>
2023-02-03 13:33:21 -0300

    feat: catch mongodb connect errors

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,13 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URL = process.env.DATABASE || 'mongodb://127.0.0.1:27017/library-server';
+const MONGODB_URL = process.env.DATABASE || 'mongodb://127.0.0.1:27017/library-db-server';
 
 const initDatabase = async () => {
-  await mongoose.connect(MONGODB_URL);
+  try {
+    await mongoose.connect(MONGODB_URL);
+  } catch (error) {
+    console.log(`mongoose connect error: ${error}`);
+  }
 };
 
 export default initDatabase;
