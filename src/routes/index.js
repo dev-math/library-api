0a5697715a6af9aa48dd4c7e8b5ffd4f6a67bed7
Matheus Rocha <01devmath@gmail.com>
2023-02-03 18:22:22 -0300

    feat: add authentication to user routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import userRoutes from './user';
 import authRoutes from './auth';
+import auth from '../middleware/auth';
 
 const router = Router();
 
-router.use('/api', userRoutes)
 router.use('/api', authRoutes)
+router.use('/api', auth, userRoutes)
 
 export default router;
