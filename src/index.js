cdc5f12f7307570cacd2e46e22bf076ef58e3c0a
Matheus Rocha <01devmath@gmail.com>
2023-02-04 03:08:03 -0300

    remove root path route

--- src/index.js
+++ src/index.js
@@ -11,20 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(routes);
 
-// BOOKS
-// GET /books - Retrieve a list of all books in the library
-// GET /books/:id - Retrieve information about a specific book
-// POST /books - Add a new book to the library
-// PUT /books/:id - Update information about a specific book
-// DELETE /books/:id - Remove a book from the library
-
-// SEARCH
-// GET /search?q=:query - Search for books by title, author, or genre
-
-app.get("/", (_req, res) => {
-  res.send("Oi");
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
