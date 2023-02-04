100555ba00c6bbb7cfeb948e78ae83fae3e289ad
Matheus Rocha <01devmath@gmail.com>
2023-02-04 02:53:29 -0300

    feat: separete auth routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,16 @@
-import { Router } from 'express';
-import userRoutes from './user';
-import authRoutes from './auth';
-import auth from '../middleware/auth';
+import { Router } from "express";
+import userRoutes from "./user";
+import authRoutes from "./auth";
+import bookRoutes from "./book";
+import auth from "../middleware/auth";
 
 const router = Router();
 
-router.use('/api', authRoutes)
-router.use('/api', auth, userRoutes)
+router.use("/api", authRoutes);
+router.use("/api", bookRoutes);
+
+// ==== authenticated routes below ====
+router.use("/api", auth);
+router.use("/api", userRoutes);
 
 export default router;
