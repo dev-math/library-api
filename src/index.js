7c9c68dc921f67bf6152cc1a948dc973f49db38c
Matheus Rocha <01devmath@gmail.com>
2023-02-13 23:46:37 -0300

    feat: enable cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
+import cors from "cors";
 import initDatabase from "./config/db";
-import routes from './routes';
+import routes from "./routes";
 
 initDatabase();
 
@@ -8,7 +9,8 @@ const app = express();
 const PORT = 8080;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(routes);
 
 app.listen(PORT, () => {
